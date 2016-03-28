460e0a2ef035d9f72d3ccffef369bf278450f64c
Parminder Singh <paramsinghvc@gmail.com>
2016-03-28 19:33:23 +0530

    NLForm

--- src/nlform.js
+++ src/nlform.js
@@ -11,17 +11,18 @@ angular.module('nlform', [])
 			}
 		}
 	})
-	.directive('nlInput', function() {
+	.directive('nlInput', ['$timeout', function($timeout) {
 		return {
 			restrict: 'AE',
 			replace: false,
 			scope: {
 				placeholder: '@',
 				type: '@',
+				pattern: '@',
 				values: '=',
 				value: '='
 			},
-			template: '<div class="overlay" ng-click="toggleVisibility()" ng-class="{\'visible\' : toggle == true}"></div><span class="filler" ng-click="toggleVisibility($event)">{{placeholder}}</span><div class="combo" ng-class="{\'visible\' : toggle == true}"><ul><li ng-if="values" ng-click="selectVal(val)" ng-repeat="val in values">{{val}}</li></ul></div><div class="textfield" ng-if="value" ng-class="{\'visible\' : toggle == true}"><form ng-submit="selectVal(txtBoxVal)"><input type="text" ng-model="txtBoxVal" placeholder="{{placeholder}}" pattern="\\d{2}\\/\\d{2}\\/\\d{4}" autofocus><button type="submit" class="submitBtn">&#8594;</button></form></div>',
+			template: '<div class="overlay" ng-click="toggleVisibility()" ng-class="{\'visible\' : toggle == true}"></div><span class="filler" ng-click="toggleVisibility($event)">{{placeholder}}</span><div class="combo" ng-class="{\'visible\' : toggle == true}"><ul><li ng-if="values" ng-click="selectVal(val)" ng-repeat="val in values">{{val}}</li></ul></div><div class="textfield" ng-if="value" ng-class="{\'visible\' : toggle == true}"><form ng-submit="selectVal(txtBoxVal)"><input type="text" ng-model="txtBoxVal" placeholder="{{placeholder}}" autofocus><button type="submit" class="submitBtn">&#8594;</button></form></div>',
 			compile: function($el, $attrs) {
 				return function($scope, $el, $attrs) {
 					$scope.toggle = false;
@@ -37,6 +38,12 @@ angular.module('nlform', [])
 						$scope.toggle = false;
 					}
 
+					$timeout(function() {
+						if (!!$scope.pattern) {
+							$el[0].querySelector("input[type=text]").setAttribute('pattern', $scope.pattern);
+						}
+					});
+
 					switch ($scope.type) {
 						case "field":
 						case "combo":
@@ -44,4 +51,4 @@ angular.module('nlform', [])
 				}
 			}
 		}
-	})
+	}])
